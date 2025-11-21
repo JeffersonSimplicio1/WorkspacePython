890fef39fb4f35ce64c0aacf2005a614ca6b144c
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-11-20 22:44:09 -0300

    Atualizando a pasta tipos de operadores
    ...

---
+++ 02-Tipos_de_operadores/01-Aritimeticos.py
@@ -0,0 +1,24 @@
+#Adição
+print(6+8)
+
+#Subtração
+print(10-5)
+
+#multiplicação
+print(8*2)
+
+#Divisão (retorna um dado float)
+print(8/2)
+
+#Divisão INTEIRA
+print(8//2)
+
+#Modulo (Retorna o resto da divisão)
+print(10%3)
+
+#Exponenciação
+print(3**2)
+
+
+
+
