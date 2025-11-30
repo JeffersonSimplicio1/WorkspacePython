8a6c08e82a774614f597ee4386b26277a860f85d
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-11-30 10:18:13 -0300

    Update 04-Estrutura_RepeticaoWHILE.py

--- 03-Estruturas_Condici_Repeti/04-Estrutura_RepeticaoWHILE.py
+++ 03-Estruturas_Condici_Repeti/04-Estrutura_RepeticaoWHILE.py
@@ -1,4 +1,4 @@
-opcao = -1
+""" opcao = -1
 
 while opcao != 0:
     opcao = int(input("Escolha uma opção: \n" \
@@ -11,4 +11,25 @@ while opcao != 0:
     elif opcao == 2:
         print("Exibindo o extrato...")
 else:
-    ("Obrigado por utilizar nossos sistemas")
+    ("Obrigado por utilizar nossos sistemas") """
+
+
+
+#Exemplo
+
+#Criar um programa que vai solicitar um numero ao usuario. Se o numero inserio for par o programa informa que o numero é par e continua executando, caso seja impar o programa informa que o numero é impar e sera encerrado.
+
+numero = -1
+
+while numero != 0:
+    numero = int(input("Digite um numero: "))
+    if numero % 2 == 0:
+        print("O numero é par!")
+        print()
+        numero = int(input("Digite um numero: "))
+        print("O numero é par!")
+        print()
+    else:
+        print("O numero é impar!\n" \
+        "      Programa finalizado!")
+        break
