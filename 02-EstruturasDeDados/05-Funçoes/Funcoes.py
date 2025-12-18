830aa73aae28e8c7d050acef75e907169987eb83
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-12-17 21:56:31 -0300

    aprendendo funções

---
+++ 02-EstruturasDeDados/05-Funçoes/Funcoes.py
@@ -0,0 +1,18 @@
+'''
+Funções
+
+Bloco de codigo identificado por um nome. Permite organização do codigo e reutilização.
+
+'''
+#Bloco 01
+def exibir_mensagem():
+    print("olá mundo!")
+
+#Bloco 02
+def exibir_mensagem2(nome):
+    print(f'Seja bem vindo {nome}')
+
+
+#Chamando as funções
+exibir_mensagem()
+exibir_mensagem2(nome = 'Jefferson')
