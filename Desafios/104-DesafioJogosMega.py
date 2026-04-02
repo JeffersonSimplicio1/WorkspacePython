332b6b153a3be05efcd5116ad9652aaff940c52d
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2026-04-02 10:22:42 -0300

    Update 104-DesafioJogosMega.py
    Melhorando o desafio sorteios da mega sena

--- Desafios/104-DesafioJogosMega.py
+++ Desafios/104-DesafioJogosMega.py
@@ -2,22 +2,36 @@
 # O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre
 # 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 import random
+from random import randint
+from time import sleep
 
-cartela =[]
-jogo =[]
-
-pergunta = int(input('Quantos jogos você deseja gerar? '))
+lista = []
+jogos = []
 cont = 0
 
-while cont < pergunta:
-    for n in range(0,6):
-        sorteio = random.randint(1,60)
-        if sorteio not in jogo:
-            jogo.append(sorteio)
-        else:
-            sorteio = random.randint(1, 60)
-            jogo.append(sorteio)
-    cartela.append(jogo[:])
-    jogo.clear()
-    cont += 1
-print(cartela)
+print('_' *30)
+print('       Jogos da Mega Sena      ')
+print('_'*30)
+quant = int(input('Quantos jogos você deseja fazer? '))
+print()
+tot = 0
+while tot< quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont +=1
+        if cont >=6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot +=1
+print('-=' *3, f'Sorteando {quant} jogos' , '-='*3)
+print()
+
+for i , valor in enumerate (jogos):
+    print(f'Jogo {i+1}: {valor}')
+    sleep(1.5)
+print('-='*5, f'BOA SORTE!!', '-='*5 )
