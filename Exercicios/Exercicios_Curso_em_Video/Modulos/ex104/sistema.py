56a0d2b588b390fd6a595d603695a187884ed05e
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2026-05-30 19:26:52 -0300

    exercicio cadastro de pessoas
    programa para cadastro de pessoas, utilizando inteface, modularização e logica de programação. Esse exercicio foi muito importante e me ajudou a aprender sobre interfaces e modularização, assim como praticar os conceitos de logica de programação

--- Exercicios/Exercicios_Curso_em_Video/Modulos/ex104/sistema.py
+++ Exercicios/Exercicios_Curso_em_Video/Modulos/ex104/sistema.py
@@ -1,13 +1,24 @@
 from time import sleep
 
 from WorkspacePython.Exercicios.Exercicios_Curso_em_Video.Modulos.ex104.lib.interface import *
+from WorkspacePython.Exercicios.Exercicios_Curso_em_Video.Modulos.ex104.lib.arquivo import *
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
-    resposta = menu(['Criar Arquivo','Cadastrar Pessoas','Sair do sistema'])
+    resposta = menu(['Ver pessoas cadastradas','Cadastrar novas Pessoas','Sair do sistema'])
     if resposta == 1:
-        print('Opção 01')
+        #Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
     elif resposta == 2:
-        print("Opção 02")
+        #Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = int(input('Idade: '))
+        cadastrar(arq,nome,idade)
     elif resposta == 3:
         print('Saindo do sistema... Até logo!')
         break
